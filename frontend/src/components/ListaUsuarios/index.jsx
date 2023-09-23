24107abe2245121b48a3be5b923b555aa089330e
laurabalves <laura.alves19@gmail.com>
2023-09-22 22:12:28 -0300

    estilizando tela de gerenciamento de usuarios

--- frontend/src/components/ListaUsuarios/index.jsx
+++ frontend/src/components/ListaUsuarios/index.jsx
@@ -1,5 +1,62 @@
 import "./styles.css";
 
 export function ListaUsuarios() {
-  return <div>lilllllsta</div>;
+  return (
+    <div className="tudo"> 
+      <div className="usuarios">
+    <h1>Permissões de usuarios</h1>
+    <br />
+    <div className="user1">
+      <p>Laura</p> 
+      <select>
+        <option value="">Administrador</option>
+        <option value="">Padrao</option>
+      </select>
+    </div>
+    <br />
+    <div className="user2">
+      <p>Matheus</p>
+      <select>
+        <option value="">Administrador</option>
+        <option value="">Padrao</option>
+      </select>
+    </div>{" "}
+    <br />
+    <div>
+      <p>Mayara</p>
+      <select>
+        <option value="">Administrador</option>
+        <option value="">Padrao</option>
+      </select>
+    </div>{" "} <br />
+
+    <div>
+      <p>Eliete</p>
+      <select>
+        <option value="">Administrador</option>
+        <option value="">Padrao</option>
+      </select>
+    </div>{" "} <br />
+
+
+    <div>
+      <p>Eliete</p>
+      <select>
+        <option value="">Administrador</option>
+        <option value="">Padrao</option>
+      </select>
+    </div>{" "} <br />
+
+    <div>
+      <p>Eliete</p>
+      <select>
+        <option value="">Administrador</option>
+        <option value="">Padrao</option>
+      </select>
+    </div>{" "}
+    <br />
+    <button className="editar">Editar</button>
+  </div></div>
+   
+  );
 }
